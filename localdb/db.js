fde2c0760f270a0ae5c0b35eba69dd34db818239
myatthu-mm <mttmmtmt@gmail.com>
2020-02-05 00:16:48 +0630

    remove db

--- localdb/db.js
+++
@@ -1,9 +0,0 @@
-const tasks = [
-    {
-        id: 1,
-        title: "lunch",
-        description: "Go for lunc by 2pm"
-    }
-];
-
-export default tasks;
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,8 @@
 import express from 'express';
 /** import routes */
-import taskRouter from '../routes/tasks';
+
 const app = express()
 
 /** use router */
-app.use('/tasks/', taskRouter)
 
 export default app;
